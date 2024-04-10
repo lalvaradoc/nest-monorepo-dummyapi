70fbb5a4ef0440b883b615c9ab8c0cf295fe92a4
lalvaradoc <lalvarado@distelsa.com.gt>
2024-04-10 15:18:57 -0600

    feat: removed unneeded apps

--- apps/myapp/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { MyappModule } from './myapp.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(MyappModule);
-  await app.listen(3000);
-}
-bootstrap();
--- apps/myapp/src/myapp.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class MyappService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
